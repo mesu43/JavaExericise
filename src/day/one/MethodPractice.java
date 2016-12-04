ee4271de6a2b0ba6eb7c8501db93404e17ac7bb7
Sumon Islam <islamsumon10@gmail.com>
2016-12-03 21:21:13 -0500

    Add a method

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -1,5 +1,27 @@
 package day.one;
 
 public class MethodPractice {
+	
+	public static void main (String[] args) {
+		
+		
+		System.out.println(getName("i am from bangladesh"));
 
+	}
+	/**Q 1.
+	 * write a method that take your name input and return your name
+	 * 
+	 *input/parameter:String name returnType:string
+	 * out put/returnType:string
+	 * servingBucket/container:variable
+	 */
+	public static String getName(String name) {
+
+		String sumonname = "";
+		sumonname = name;
+
+		return sumonname;
+	
+	}
 }
+
