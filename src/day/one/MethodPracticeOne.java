cc93829d1912679b67e45d6e16df196f8dcbca4f
Sumon Islam <islamsumon10@gmail.com>
2016-12-11 01:09:34 -0500

    add new method two

--- src/day/one/MethodPracticeOne.java
+++ src/day/one/MethodPracticeOne.java
@@ -1,12 +1,38 @@
 package day.one;
 
+import java.util.ArrayList;
+
 public class MethodPracticeOne {
 
 	public static void main(String[] args) {
 
 		System.out.println(getName("i am from bangladesh"));
+		
+		System.out.println(getEvenNumberlist(16));
 
 	}
+	
+	/**Q.number.2
+	 * 
+	 * write a method that take a number input and return the list of all even
+	 * number from 0 to that given number.
+	 *
+	 * parameter/input:int number
+	 * returnType/output:ArrayList<Integer>
+	 * servingBucket/container:List
+	 */ 
+	
+	public static ArrayList<Integer> getEvenNumberlist(int number){
+		ArrayList<Integer>  givenNumberlist = new ArrayList<Integer>();
+		
+		for(int i = 0; i<=number; i++){
+			if(i % 2 ==0){
+				 givenNumberlist.add(i);
+			}
+		}
+		return givenNumberlist;
+	}
+	 
 
 	/**
 	 * Q 1. write a method that take your name input and return your name
