ebf8b4948f834212a81b798c0acf935e0beb5483
Sumon Islam <islamsumon10@gmail.com>
2016-12-22 15:26:38 -0500

    Exercise for new method number#5

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -5,11 +5,24 @@ import java.util.ArrayList;
 public class MethodPractice {
 	
 	public static void main(String[] args){
+		System.out.println("Ans#5." + getUpperCase("this is a String"));
 		System.out.println("Ans#4." + getAsKilometer(10));
 		System.out.println("Ans#3." + getAllOddNumber(10));
 		System.out.println("Ans#2." + getAllEvenNumber(20));
 		System.out.println("Ans#1. " + getName("This is a String"));
 		
+	}
+	/**Q.5
+	 * write a method that take one String input and return same String in upper case.
+	 * 
+	 * parameter/input:String text.
+	 * returnType/output:String.
+	 * servingBucket/container:variable.
+	 */
+	public static String getUpperCase(String text) {
+		String uppercase = "";
+		uppercase = text.toUpperCase();
+		return uppercase;
 	}
 	/**Q.4
 	 * write a method that take input as mile & return as kilometer.
